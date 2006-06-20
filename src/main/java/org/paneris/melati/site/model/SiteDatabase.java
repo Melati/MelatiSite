daaba48f2587ad2376f5a41d2034e0d0be68a0cd
Tim Pizey <timp@paneris.org>
2006-06-20 09:03:49 +0000

    Ensure home page

--- src/main/java/org/paneris/melati/site/model/SiteDatabase.java
+++ src/main/java/org/paneris/melati/site/model/SiteDatabase.java
@@ -1,5 +1,7 @@
 package org.paneris.melati.site.model;
 
+import org.melati.poem.AccessToken;
+import org.melati.poem.PoemTask;
 import org.paneris.melati.site.model.generated.SiteDatabaseBase;
 
 /**
@@ -8,6 +10,32 @@ import org.paneris.melati.site.model.generated.SiteDatabaseBase;
 public class SiteDatabase extends SiteDatabaseBase
                             implements SiteDatabaseTables {
   // programmer's domain-specific code here
+
+  private Page homePage;
+  
+  public void connect(
+      String dbmsclass,
+      String url,
+      String username,
+      String password,
+      int maxConnections) {
+      super.connect(dbmsclass, url, username, password, maxConnections);
+
+      inSession(AccessToken.root, new PoemTask() {
+        public void run() {
+
+          homePage = (Page)getPageTable().ensure("Home");
+
+        }
+      });
+  }
+
+  /**
+   * @return Returns the homePage.
+   */
+  public Page getHomePage() {
+    return homePage;
+  }
 }
 
 
